46c75439e81565a737123483ec8d244fc7d04fa0
raunakjr <raunakmishra9798@gmail.com>
2025-01-25 21:25:06 +0530

    added express codes

--- Express/script.js
+++ Express/script.js
@@ -3,7 +3,18 @@ const express = require("express");
 // getting all functionalty of express by calling it
 const app = express();
 
+// way to use middleware
+// this will run first before going to url
+// app.use(function (req, res, next) {
+//   console.log("middleware chala");
+//   next();
+// });
+
 // Now we can create Routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.send("Hii this is Express Server");
 });
